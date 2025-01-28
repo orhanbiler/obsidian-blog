a9580f8174fda93106254a5f2d5f3581d18f63fb
Orhan G. Biler <bilergunes@gmail.com>
2025-01-28 02:20:59 -0500

    Update author bios and add Vercel Analytics to App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, ChakraProvider } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react';
+import theme from './theme';
 import AppRoutes from './AppRoutes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <Flex direction="column" minH="100vh">
-      <Navbar />
-      <Box flex="1">
-        <AppRoutes />
-      </Box>
-      <Footer />
-    </Flex>
+    <ChakraProvider theme={theme}>
+      <Flex direction="column" minH="100vh">
+        <Navbar />
+        <Box flex="1">
+          <AppRoutes />
+        </Box>
+        <Footer />
+      </Flex>
+      <Analytics />
+    </ChakraProvider>
   );
 }
 
